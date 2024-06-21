f9c07b7411b055336f85dba463033a15b37a3dce
origin <maryna.oganesyan@yandex.ru>
2024-06-21 10:31:13 +0400

    Я испарвила,ваши замечания

--- src/HogwartsStudent.java
+++ src/HogwartsStudent.java
@@ -26,6 +26,18 @@ private final int transgressionDistance;
     public int getTransgressionDistance() {
         return transgressionDistance;
     }
-    public void compareStudentWith(HogwartsStudent other) {
+    public void compareStudentWith (HogwartsStudent student1,HogwartsStudent student2) {
+        int student1Power = student1.magicPower + student1.transgressionDistance;
+        int student2Power = student2.magicPower + student2.transgressionDistance;
+        if (student1Power > student2Power) {
+            System.out.printf("%s обладает бельшей мощностью магии,чем %s",
+                    student1.getName(),student2.getName());
+        } else if (student1Power < student2Power) {
+            System.out.printf("%s обладает большей мощностью магии,чем %s",
+                    student2.getName(),student1.getName());
+        } else {
+            System.out.printf("%s и %s обладает равносильной мощностью мигии",
+                    student1.getName(),student2.getName());
+        }
     }
 }
